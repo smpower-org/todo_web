0426101b5cd188de0ce8e4e1ec0f856e41d0d996
smpower <rf.wangchn@gmail.com>
2018-11-06 15:43:10 +0800

    add footer component

---
+++ src/footer/views/footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './style.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">Footer Component</div>
+    );
+  }
+}
+
+export default Footer;
