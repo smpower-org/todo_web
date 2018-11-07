9c99133f7755056ef34f716baf87e1a570392e2a
smpower <rf.wangchn@gmail.com>
2018-11-07 14:53:30 +0800

    update;

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import TestFetch from './testFetch';
+// import TestFetch from './testFetch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+	{/*<header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -21,7 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
-	<TestFetch />
+	<TestFetch />*/}
       </div>
     );
   }
