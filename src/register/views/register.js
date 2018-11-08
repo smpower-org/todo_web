96108f09675e5aa85a8080b97133ba0ec73b30b8
smpower <rf.wangchn@gmail.com>
2018-11-08 14:16:55 +0800

    更新 Register 组件结构及样式

--- src/register/views/register.js
+++ src/register/views/register.js
@@ -5,7 +5,45 @@ import './style.css';
 class Register extends Component {
   render() {
     return (
-      <div className="register">Register Component</div>
+      <div className="main">
+	<div className="register">
+	  <div className="register-logo">
+	    <i className="register-logo-icon"></i>
+	    <h1 className="register-logo-title">Get Wundertodo For Free</h1>
+	  </div>
+	  <div className="register-inner">
+	    <div className="register-item register-name">
+	      <i className="register-item-icon register-name-icon"></i>
+	      <input
+		type="text"
+		className="register-item-input register-name-input"
+		placeholder="Name" 
+	      />
+	    </div>
+	    <div className="register-item register-email">
+	      <i className="register-item-icon register-email-icon"></i>
+	      <input
+		type="text"
+		className="register-item-input register-email-input"
+		placeholder="Email" 
+	      />
+	    </div>
+	    <div className="register-item register-password">
+	      <i className="register-item-icon register-password-icon"></i>
+	      <input
+		type="text"
+		className="register-item-input register-password-input"
+		placeholder="Password" 
+	      />
+	    </div>
+	    <div className="register-item register-submit">
+	      <button type="button" className="register-submit-btn">
+	        Create Free Account
+	      </button>
+	    </div>
+	  </div>
+	</div>
+      </div>
     );
   }
 }
