93286cbdd0dbea44616de9a409ba325d06bd72bc
smpower <rf.wangchn@gmail.com>
2018-11-19 17:13:59 +0800

    编写导航菜单中的用户自定义列表基本结构和对应样式

--- src/navigation/views/listsToolbar/views/collection.js
+++ src/navigation/views/listsToolbar/views/collection.js
@@ -4,7 +4,21 @@ class Collection extends Component {
   render() {
     return (
       <ul className="lists-toolbar-collection">
-	<li></li>
+	<li className="lists-toolbar-collection-item">
+	  <i className="lists-toolbar-collection-item-icon"></i>
+	  <span className="lists-toolbar-collection-item-title">test list 1</span>
+	  <span className="lists-toolbar-collection-item-count">2</span>
+	</li>
+	<li className="lists-toolbar-collection-item">
+	  <i className="lists-toolbar-collection-item-icon"></i>
+	  <span className="lists-toolbar-collection-item-title">test list 2</span>
+	  <span className="lists-toolbar-collection-item-count">4</span>
+	</li>
+	<li className="lists-toolbar-collection-item">
+	  <i className="lists-toolbar-collection-item-icon"></i>
+	  <span className="lists-toolbar-collection-item-title">test list 2</span>
+	  <span className="lists-toolbar-collection-item-count">7</span>
+	</li>
       </ul>
     );
   }
