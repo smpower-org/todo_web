d5e86e449da44488966680d492201c4ee3ee3723
smpower <rf.wangchn@gmail.com>
2019-03-29 17:29:13 +0800

    eject; 编写【更多】模块

--- src/content/views/header.js
+++ src/content/views/header.js
@@ -3,19 +3,33 @@ import React, { Component } from 'react';
 import sortSvg from './images/sort.svg';
 import moreSvg from './images/more.svg';
 import copySvg from './images/copy.svg';
+import emailSvg from './images/email.svg';
+import printSvg from './images/print.svg';
+import undisturbSvg from './images/undisturb.svg';
+import deleteSvg from './images/delete.svg';
 
 class Header extends Component {
+  constructor() {
+    super();
+
+    this.onClickMore = this.onClickMore.bind(this);
+
+    this.state = {
+      isMoreContentExtracted: false,
+    };
+  }
+
   render() {
     return(
       <div className="content-header">
 	<h1>收件箱</h1>
 	<div className="action-bar">
 	  <div className="action-bar-buttons">
-	    <button className="sort hidden" onClick={this.onSort}>
+	    <button className="sort hidden" onClick={this.onClickSort}>
 	      <img src={sortSvg} alt="排序" />
 	      <span>排序</span>
 	    </button>
-	    <button className="more" onClick={this.onMore}>
+	    <button className="more" onClick={this.onClickMore}>
 	      <img src={moreSvg} alt="更多" />
 	      <span>更多</span>
 	    </button>
@@ -23,10 +37,32 @@ class Header extends Component {
 	  <div className="action-bar-content">
 	    <ul className="more">
 	      <li>
-		<a href="#">
-		  <img src={copySvg} alt="复制" />
-		  <span>复制清单</span>
-		</a>
+		<img src={copySvg} alt="复制" />
+		<span>复制清单</span>
+	      </li>
+	      <li>
+	        <img src={emailSvg} alt="以电子邮件发送清单" />
+		<span>以电子邮件发送清单</span>
+	      </li>
+	      <li>
+	        <img src={emailSvg} alt="用电子邮件发送所选任务" />
+		<span>用电子邮件发送所选任务</span>
+	      </li>
+	      <li>
+	        <img src={printSvg} alt="打印清单" />
+		<span>打印清单</span>
+	      </li>
+	      <li>
+	        <img src={printSvg} alt="打印所选任务" />
+		<span>打印所选任务</span>
+	      </li>
+	      <li>
+	        <img src={undisturbSvg} alt="勿扰" />
+		<span>勿扰</span>
+	      </li>
+	      <li>
+	        <img src={deleteSvg} alt="删除" />
+		<span>删除所选任务</span>
 	      </li>
 	    </ul>
 	  </div>
@@ -35,12 +71,15 @@ class Header extends Component {
     );
   }
 
-  onSort() {
+  onClickSort() {
     console.log('on sort...');
   }
 
-  onMore() {
-    console.log('on more...');
+  onClickMore() {
+    console.log('on click more button...');
+    this.setState({
+      isMoreContentExtracted: !this.state.isMoreContentExtracted,
+    });
   }
 }
 
