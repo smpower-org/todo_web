10b6abfc23d0e5174e65405f79c001d4136b969f
smpower <rf.wangchn@gmail.com>
2019-04-04 11:55:16 +0800

    调整 404 页面结构、样式

--- src/404/views/404.js
+++ src/404/views/404.js
@@ -6,12 +6,11 @@ import './style.scss';
 class NoMath extends Component {
   render() {
     return (
-      <div className="no-match">
-	<p>
-	  页面不见了，返回
-	</p>
-	<Link to="/home">首页</Link>
-	<span ref="s"></span>
+      <div className="no-match-box">
+	<div className="no-match">
+	  <span>页面不见了，返回</span>&nbsp;
+	  <Link to="/home">首页</Link>
+	</div>
       </div>
     );
   }
