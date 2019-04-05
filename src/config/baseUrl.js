ea1f167ff1f2af534700a3b92fe4c9d53eb6cba0
smpower <rf.wangchn@gmail.com>
2019-04-06 00:09:55 +0800

    update

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,4 +1,5 @@
 // const baseUrl = 'https://www.wundertodo.xyz:1116';
-const baseUrl = 'http://localhost:1115';
+// const baseUrl = 'http://localhost:1115';
+const baseUrl = 'http://47.110.53.31:1115';
 
 export default baseUrl;
