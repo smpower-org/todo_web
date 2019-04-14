a0d605925ee0036ad9395a8e1f4674d3d5a13b4e
rf.wangchn <rf.wangchn@gmail.com>
2019-04-14 21:49:05 +0800

    add taskBox component

---
+++ src/components/taskBox/views/taskBox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './style.scss';
+
+class TaskBox extends Component {
+  render() {
+    return (
+      <div className="task-box">
+        task box
+      </div>
+    );
+  }
+}
+
+export default TaskBox;
+
