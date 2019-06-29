4bf8ea80d16654464a25d5e6260d21a0d0c7a58c
smpower <rf.wangchn>
2019-06-30 01:07:51 +0800

    eject and modified webpackConfig file

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootAction from './actions'
 import reducers from './reducers'
@@ -8,7 +7,7 @@ import reducers from './reducers'
 const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
   reducers,
-  composeWithDevTools(applyMiddleware(thunkMiddleware, sagaMiddleware))
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 )
 sagaMiddleware.run(rootAction)
 
