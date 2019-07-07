4c6aabb0b734cef181cefca5be963fe2dc4de1db
王若非.ex <wangruofei.ex@hx-partners.com>
2019-07-07 15:43:56 +0800

    added util: Validator

---
+++ src/utils/Validator/strategies.js
@@ -0,0 +1,27 @@
+const strategies = {
+  isNonEmpty(value, errorMsg) {  // 验证是否为空
+    if (value === '') return errorMsg
+  },
+  minLength(value, length, errorMsg) {  // 验证最小长度
+    if (value.length < length) return errorMsg
+  },
+  maxLength(value, length, errorMsg) {  // 验证最大长度
+    if (value.length > length) return errorMsg
+  },
+  isMobile(value, errorMsg) {  // 验证是否为 11 位手机号码
+    if (!/(^1[3|5|8][0-9]{9}$)/g.test(value)) return errorMsg
+  },
+  isEmail(value, errorMsg) {
+    if (!/^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/g.test(value)) {  // 验证邮箱是否合法
+      return errorMsg
+    }
+  },
+  isUsernameExisted(value, errorMsg) {  // 用户注册时，判断用户名是否已存在
+    if (typeof errorMsg === 'function') errorMsg(value)
+  },
+  isEmailExisted(value, errorMsg) {  // 用户注册时，判断用户邮箱是否已存在
+    if (typeof errorMsg === 'function') errorMsg(value)
+  }
+};
+
+export default strategies
---
+++ src/utils/Validator/validator.js
@@ -0,0 +1,33 @@
+import strategies from './strategies'
+
+class Validator {
+  constructor() {
+    this.cache = []
+  }
+
+  add(value, rules) {  // 添加验证规则
+    const self = this  // 保存当前 this
+
+    rules.forEach((item, index) => {
+      (item => {
+        const strategyArr = item.strategy.split(':')  // 把 strategy 和参数分开
+        const errorMsg = item.errorMsg
+        self.cache.push(() => {
+          const strategy = strategyArr.shift().trim()  // 用户挑选的 strategy
+          strategyArr.unshift(value.trim())  // 把 value 值添加经参数列表
+          strategyArr.push(errorMsg)  // 把 errorMsg 添加进参数列表
+          return strategies[strategy](...strategyArr)
+        });
+      })(item)
+    });
+  }
+
+  start() {  // 开始校验
+    for (let i = 0, len = this.cache.length; i < len; i++) {
+      const msg = this.cache[i]()
+      if (msg) return msg  // 若 msg 存在，则说明校验未通过
+    }
+  }
+}
+
+export default Validator
