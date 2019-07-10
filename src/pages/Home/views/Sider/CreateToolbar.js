fc667bd10cc7ea07ec39b5a6c74013781518a7a1
smpower <rf.wangchn@gmail.com>
2019-07-11 00:04:31 +0800

    added component: Home/Sider/CreateToolbar

--- src/pages/Home/views/Sider/CreateToolbar.js
+++ src/pages/Home/views/Sider/CreateToolbar.js
@@ -3,7 +3,12 @@ import React from 'react'
 class CreateToolbar extends React.Component {
   render() {
     return (
-      <div>CreateToolbar component.</div>
+      <div className="create-toolbar">
+        <div className="create-toolbar-inner" onClick={this.showModal}>
+          <i className="create-toolbar-icon"></i>
+          <span className="create-toolbar-title">创建清单</span>
+        </div>
+      </div>
     )
   }
 }
--- src/pages/Home/views/Sider/ListsToolbar.js
+++ src/pages/Home/views/Sider/ListsToolbar.js
@@ -3,7 +3,9 @@ import React from 'react'
 class ListsToolbar extends React.Component {
   render() {
     return (
-      <div>ListsToolbar component.</div>
+      <div className="lists-toolbar">
+        ListsToolbar component.
+      </div>
     )
   }
 }
